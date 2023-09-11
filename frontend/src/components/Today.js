279344941e899224804d01f12397cb55ef22bf36
Jose Jimenez <joseluisjimagui@gmail.com>
2023-09-10 23:36:06 -0600

    Install package chart.js

--- frontend/src/components/Today.js
+++ frontend/src/components/Today.js
@@ -4,10 +4,19 @@ import axios from 'axios'
 export default class 
 Today extends Component {
 
+  state = {
+    currentState : ''
+  }
+
   async componentDidMount(){
     const res = await axios.get(' https://api.covidtracking.com/v1/states/current.json')
-    //console.log(res)
+    console.log(res.data[0])
+
+
+
   }
+
+  
 
   render() {
     return (
