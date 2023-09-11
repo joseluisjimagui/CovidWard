d530d1a56374b5b9db1e212788ed182093cd067d
Jose Jimenez <joseluisjimagui@gmail.com>
2023-09-11 13:45:50 -0600

    Add Navbar

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,10 +1,12 @@
 import './App.css';
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import Today from './components/Today';
+import Navbar from './components/Navbar';
 
 function App() {
   return (
     <div>
+      <Navbar/>
       <BrowserRouter>
         <Routes>
           <Route path="/" element={<Today />} />          
