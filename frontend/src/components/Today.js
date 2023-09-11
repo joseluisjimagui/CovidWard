ebab226b12d5e4a32b8515dd005178db99fc78d4
Jose Jimenez <joseluisjimagui@gmail.com>
2023-09-10 23:51:17 -0600

    Install package react-chart.js-2

--- frontend/src/components/Today.js
+++ frontend/src/components/Today.js
@@ -1,28 +1,59 @@
 import React, { Component } from 'react'
+import { Chart } from 'chart.js'
 import axios from 'axios'
 
-export default class 
-Today extends Component {
+export default class
+  Today extends Component {
 
   state = {
-    currentState : ''
+    currentState: ''
   }
 
-  async componentDidMount(){
+  async componentDidMount() {
     const res = await axios.get(' https://api.covidtracking.com/v1/states/current.json')
-    console.log(res.data[0])
+    this.checkRes(res.data[0])
 
+    const ctx = document.getElementById('myChart');
 
+    new Chart(ctx, {
+      type: 'bar',
+      data: {
+        labels: ['Red', 'Blue', 'Yellow', 'Green', 'Purple', 'Orange'],
+        datasets: [{
+          label: '# of Votes',
+          data: [12, 19, 3, 5, 2, 3],
+          borderWidth: 1
+        }]
+      },
+      options: {
+        scales: {
+          y: {
+            beginAtZero: true
+          }
+        }
+      }
+    });
 
   }
 
-  
+  checkRes(res) {
+    this.state.currentState = res.state
+    delete res.state
+    delete res.dateChecked
+    delete res.dateModified
+    delete res.date
+    delete res.hash
+    console.log(res)
+
+  }
 
   render() {
     return (
       <div>
-        Today
+        <canvas id="myChart"></canvas>
       </div>
+
+      
     )
   }
 }
