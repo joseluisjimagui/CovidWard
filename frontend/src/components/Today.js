4acffba6389b80aff572f54916a99601c491a895
Jose Jimenez <joseluisjimagui@gmail.com>
2023-09-10 23:07:37 -0600

    Install axios

--- frontend/src/components/Today.js
+++ frontend/src/components/Today.js
@@ -1,7 +1,14 @@
 import React, { Component } from 'react'
+import axios from 'axios'
 
 export default class 
 Today extends Component {
+
+  async componentDidMount(){
+    const res = await axios.get(' https://api.covidtracking.com/v1/states/current.json')
+    //console.log(res)
+  }
+
   render() {
     return (
       <div>
